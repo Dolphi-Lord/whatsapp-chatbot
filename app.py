902bc294427fece60175805031b536aaa47119ac
Dolphi-Lord <dolphichat@gmail.com>
2025-07-03 12:25:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -207,5 +207,9 @@ def register_student():
     ref.set({'department': department})
     return jsonify({'message': 'Student registered.'}), 200
 
+@app.route('/healthz', methods=['GET'])
+def health_check():
+    return 'OK', 200
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
